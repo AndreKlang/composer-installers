6e26578bdcea652dc2fefdbe0b61e50044d38df5
Andre Klang <andre.klang@ecompartner.com>
2015-09-16 10:32:15 +0200

    Changed install-path

--- src/Aoepeople/ComposerInstallers/MagentoInstaller.php
+++ src/Aoepeople/ComposerInstallers/MagentoInstaller.php
@@ -4,7 +4,7 @@ namespace Aoepeople\ComposerInstallers;
 class MagentoInstaller extends BaseInstaller
 {
     protected $locations = array(
-        'module'   => '.modman/{$name}/',
+        'module'   => 'public_html/.modman/{$vendor}_{$name}/',
         'source'    => 'htdocs/'
     );
 }
